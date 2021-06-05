d3a878e1422bcf9904c1439462c9f3c995bf7ea0
millagmc <milla.revolts@gmail.com>
2021-06-05 15:30:46 -0300

    corpo basicao do whatsapp

---
+++ whatsapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+         <div>corpo da mensagem </div>
+          <div>
+            <button>Usuario</button>
+            <input type='text'>Mensagem </input>
+            <button>Enviar</button>
+          </div>
+          
+    </div>
+
+  );
+}
+
+export default App;
