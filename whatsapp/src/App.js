f3df6d3af05bc3043cdaafc9cc426a25100b4901
millagmc <milla.revolts@gmail.com>
2021-06-05 15:44:44 -0300

    lucas realizacoes

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/header/Header'
 
 function App() {
   return (
     <div>
-         <div>corpo da mensagem </div>
-          <div>
-            <button>Usuario</button>
-            <input type='text'>Mensagem </input>
-            <button>Enviar</button>
-          </div>
-          
+      <Header/>
+      <div>corpo da mensagem </div>
+      <div>
+        <button>Usuario</button>
+        <input type='text'>Mensagem </input>
+        <button>Enviar</button>
+      </div>          
     </div>
-
   );
 }
 
